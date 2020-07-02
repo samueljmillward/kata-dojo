ca8a4f6731ae4763b79e3e96df1704cc0c484678
samueljmillward <samuel.j.pearce@hotmail.com>
2020-07-02 13:25:41 +0100

    02-07-2020

--- 01-07-2020/alternate-capitalization.js
+++ 01-07-2020/alternate-capitalization.js
@@ -1,8 +1,13 @@
-function capitalize(s) {
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+// The input will be a lowercase string with no spaces.
 
+function capitalize(s) {
     var chars = s.toLowerCase().split("");
+
     for (var i = 0; i < chars.length; i += 2) {
         chars[i] = chars[i].toUpperCase();
     }
+
     return chars.join("");
 };
