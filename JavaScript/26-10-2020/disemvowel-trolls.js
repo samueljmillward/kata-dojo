b1726860e468fcb669d1e8204b0a57857ea4323b
samueljmillward <samuel.j.pearce@hotmail.com>
2020-10-26 23:09:56 +0000

    26-10-2020

---
+++ JavaScript/26-10-2020/disemvowel-trolls.js
@@ -0,0 +1,16 @@
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+function disemvowel(str) {
+    let splitStr = str.split("");
+    let vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    for (i = 0; i < str.length; i++) {
+      for (j = 0; j < vowel.length; j++) {
+        if(splitStr[i] === vowel[j]) {
+          splitStr.splice(i,1,"")
+          }
+      }
+    }
+    var str = splitStr.join("");
+    
+    return str;
+  }
