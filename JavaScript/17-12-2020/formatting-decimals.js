9ef12ab77872d096cc9474913e5d70ef98a3570b
samueljmillward <samuel.j.pearce@hotmail.com>
2020-12-17 23:48:21 +0000

    17-12-2020

---
+++ JavaScript/17-12-2020/formatting-decimals.js
@@ -0,0 +1,7 @@
+// Each floating-point number should be formatted that only the first two decimal places are returned. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// Don't round the numbers! Just cut them after two decimal places!
+
+function twoDecimalPlaces(number) {
+  return parseInt(number * 100) / 100;
+}
