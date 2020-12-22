811d32a06d9e56b96deeb7b85a4db45008215ceb
samueljmillward <samuel.j.pearce@hotmail.com>
2020-12-22 10:13:12 +0000

    22-12-2020

--- JavaScript/22-12-2020/get-initials.js
+++ JavaScript/22-12-2020/get-initials.js
@@ -1,6 +1,13 @@
+// Write function toInitials returs initials for a given person name. E.g: "Bill Gates" -> "B. G."
+
 function toInitials(name) {
   return name
     .split(' ')
     .map((i) => i[0] + '.')
     .join(' ');
+}
+
+// RegEx
+function toInitials(s) {
+  return s.replace(/[a-z]+/g, '.');
 }
