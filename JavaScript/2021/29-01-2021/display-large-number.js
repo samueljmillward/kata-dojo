67e1c150049e13cdc4497734706da93d1ceb4242
samueljmillward <samuel.j.pearce@hotmail.com>
2021-01-30 23:56:07 +0000

    30-01-2021

--- JavaScript/2021/29-01-2021/display-large-number.js
+++ JavaScript/2021/29-01-2021/display-large-number.js
@@ -1,3 +1,7 @@
+// In some locales (like the US) large numbers are displayed with commas every 3 digits to help users understand the size of the number at a glance.
+
+// You're going to write a function to format a number for display in that way.
+
 function displayLargeNumber(n) {
   let arr = [];
   let str = n.toString().split('').reverse().join('');
