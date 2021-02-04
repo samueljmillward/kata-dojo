56e45566c2e94ddc2c7bd962347ea42d2c6cce78
samueljmillward <samuel.j.pearce@hotmail.com>
2021-02-04 23:57:06 +0000

    04-02-2021

---
+++ JavaScript/2021/04-02-2021/lost-numbers.js
@@ -0,0 +1,7 @@
+// Numbers are lost somewhere in the arrays, help them find each other!
+
+// In this kata you'll be given 2 n-deep arrays with one number somewhere in there (or no number, just empty n-deep array). Write a function, that finds those numbers and return their sum
+
+const findAndSumm = (arr1, arr2) => {
+  return arr1.toString() * 1 + arr2.toString() * 1;
+};
