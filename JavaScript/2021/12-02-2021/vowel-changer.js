29403eba5cefcd48b2622b5c62d4d284ae92d4cf
samueljmillward <samuel.j.pearce@hotmail.com>
2021-02-12 20:23:32 +0000

    12-02-2021

---
+++ JavaScript/2021/12-02-2021/vowel-changer.js
@@ -0,0 +1,5 @@
+// Create a function that changes all the vowels (excluding y) in a string, and changes them all to the same vowel. The first parameter of the function is the string, and the second is the vowel that all the vowels in the string are being changed to.
+
+function vowelChange(str, vow) {
+  return str.replace(/[aeiou]/gi, vow);
+}
