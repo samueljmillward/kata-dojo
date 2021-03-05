3cf5815165aeb199df3070a21fff84964af5a93c
samueljmillward <samuel.j.pearce@hotmail.com>
2021-03-05 16:13:42 +0000

    05-03-2021

--- JavaScript/2021/05-03-2021/easy-time-convert.js
+++ JavaScript/2021/05-03-2021/easy-time-convert.js
@@ -1,3 +1,6 @@
+// This kata requires you to convert minutes (int) to hours and minutes in the format hh:mm (string).
+// If the input is 0 or negative value, then you should return "00:00"
+
 function timeConvert(num) {
   if (num < 0) return '00:00';
   let hours = Math.floor(num / 60);
